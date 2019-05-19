f229b5e776d4dfcca3d0ef4688bcc101acc037dc
ytyaru <ytyaru@outlook.jp>
2019-05-19 11:52:58 +0900

    ループ

--- src/guessing_game/src/main.rs
+++ src/guessing_game/src/main.rs
@@ -6,18 +6,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("Secret number is {}", secret_number);
 
-    println!("1〜100までの値を入力してください。");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("入力エラー。read_line()で失敗しました。");
-    println!("入力値: {}", guess);
+    loop {
+        println!("1〜100までの値を入力してください: ");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("入力エラー。read_line()で失敗しました。");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("数値を入力してください！");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("小さすぎます。残念……"),
-        Ordering::Greater => println!("大きすぎます。残念……"),
-        Ordering::Equal => println!("ぴったり！　やったね！"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎます！"),
+            Ordering::Greater => println!("大きすぎます！"),
+            Ordering::Equal => { 
+                println!("ぴったり！　やったね！");
+                break;
+            }
+        }
     }
 }
